249e5c42c4d12c348ee1faf7c94817d4ba99b94e
Mykyta Krumalenko <prikol769@gmail.com>
2024-07-26 13:06:19 -0500

    added  getMovie func fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import Form from "./components/Form";
 import MovieDisplay from "./components/MovieDisplay";
 
 function App() {
+  console.log(import.meta.env.VITE_API_KEY);
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (searchTerm) => {
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${
+        import.meta.env.VITE_API_KEY
+      }&t=${searchTerm}`
+    );
+    const data = await response.json();
+    setMovie(data);
+  };
+
   return (
     <>
       <h1>React Movie App</h1>
-      <Form />
+      <Form movieSearch={getMovie} />
       <MovieDisplay />
     </>
   );
