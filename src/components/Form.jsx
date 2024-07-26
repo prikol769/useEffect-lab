ede4e27f1e2ba8d629910dfa5a620338f04362d7
Mykyta Krumalenko <prikol769@gmail.com>
2024-07-26 13:06:02 -0500

    added form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,25 @@
-const Form = () => {
+import { useState } from "react";
+
+const Form = ({ movieSearch }) => {
+  const [formData, setFormData] = useState({ searchTerm: "" });
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    movieSearch(formData.searchTerm);
+  };
   return (
     <div>
-      <form action="">
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="searchTerm"
+          value={formData.searchTerm}
+          onChange={handleChange}
+        />
         <input type="submit" value="submit" />
       </form>
     </div>
