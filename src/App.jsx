6818568c40633fcb636794b203730e2c6bf2f124
Mykyta Krumalenko <prikol769@gmail.com>
2024-07-26 15:21:54 -0500

    added MovieDisplay component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "./components/Form";
 import MovieDisplay from "./components/MovieDisplay";
 
 function App() {
   const [movie, setMovie] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getMovie = async (searchTerm) => {
-    const response = await fetch(
-      `https://www.omdbapi.com/?apikey=${
-        import.meta.env.VITE_API_KEY
-      }&t=${searchTerm}`
-    );
-    const data = await response.json();
-    setMovie(data);
+    try {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://www.omdbapi.com/?apikey=${
+          import.meta.env.VITE_API_KEY
+        }&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
   };
+
+  useEffect(() => {
+    getMovie("shrek");
+  }, []);
 
   return (
     <>
       <h1>React Movie App</h1>
       <Form movieSearch={getMovie} />
-      <MovieDisplay />
+      <MovieDisplay movie={movie} isLoading={isLoading} />
     </>
   );
 }
--- src/components/MovieDisplay.jsx
+++ src/components/MovieDisplay.jsx
@@ -1,5 +1,20 @@
-const MovieDisplay = () => {
-  return <div>MovieDisplay</div>;
+const MovieDisplay = ({ movie, isLoading }) => {
+  if (isLoading) {
+    return <h1>...Loading</h1>;
+  }
+
+  if (movie?.Response === "False") {
+    return <h1>{movie?.Error}</h1>;
+  }
+
+  return (
+    <div>
+      <h1>{movie?.Title}</h1>
+      <h2>{movie?.Genre}</h2>
+      <img src={movie?.Poster} alt={movie?.Title} />
+      <h2>{movie?.Year}</h2>
+    </div>
+  );
 };
 
 export default MovieDisplay;
